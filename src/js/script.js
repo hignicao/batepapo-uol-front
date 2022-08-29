68940302659817cf6823d95d29f471a20b47e99c
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2022-08-28 23:06:16 -0300

    4th commit (functional project + bonus)

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,15 @@
 let messages = [];
 let users = [];
 let myname = "";
+let messageRecipient = "Todos";
+let messageVisibility = "message";
 let input = document.getElementById("my-message");
 
-input.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    document.getElementById("myBtn").click();
-  }
+input.addEventListener("keypress", function (event) {
+	if (event.key === "Enter") {
+		event.preventDefault();
+		document.getElementById("myBtn").click();
+	}
 });
 
 function addUserName() {
@@ -42,23 +44,23 @@ function participantsArrived(data) {
 function renderUsers() {
 	const user = document.querySelector(".choose-contact");
 	user.innerHTML = `
-	<div class="contacts">
-	<div class="ion-icon-p">
-	<ion-icon name="people-circle"></ion-icon>
-	<p>Todos</p>
-	</div>
-	<ion-icon class="check" name="checkmark-outline"></ion-icon>
+	<div onclick="changeRecipient(this)" class="contacts">
+		<div class="ion-icon-p">
+			<ion-icon name="people-circle"></ion-icon>
+			<p class="recipient-type">Todos</p>
+		</div>
+		<ion-icon class="check" name="checkmark-outline"></ion-icon>
 	</div>	
 	`;
 
 	for (let i = 0; i < users.length; i++) {
 		user.innerHTML += `
-		<div class="contacts">
-		<div class="ion-icon-p">
-		<ion-icon name="person-circle"></ion-icon>
-		<p>${users[i].name}</p>
-		</div>
-		<ion-icon class="check hidden" name="checkmark-outline"></ion-icon>
+		<div data-identifier="participant" onclick="changeRecipient(this)" class="contacts">
+			<div class="ion-icon-p">
+				<ion-icon name="person-circle"></ion-icon>
+				<p class="recipient-type" >${users[i].name}</p>
+			</div>
+			<ion-icon class="hidden" name="checkmark-outline"></ion-icon>
 		</div>	
 		`;
 	}
@@ -77,9 +79,9 @@ function sendMessage() {
 
 	const newMessage = {
 		from: myname,
-		to: "Todos",
+		to: messageRecipient,
 		text: message.value,
-		type: "message",
+		type: messageVisibility,
 	};
 
 	const promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", newMessage);
@@ -94,44 +96,43 @@ function getMessages() {
 	promisse.then(messagesArrived);
 }
 
-
 function messagesArrived(data) {
 	messages = data.data;
-	
+
 	renderMessages();
 }
 
 function renderMessages() {
 	const message = document.querySelector(".messages");
 	message.innerHTML = "";
-	
+
 	for (let i = 0; i < messages.length; i++) {
 		if (messages[i].type === "message") {
 			message.innerHTML += `
 			<li class="message">
-			<span class="message-time">(${messages[i].time})</span>
-			<span class="message-from">${messages[i].from}</span>
-			<span>para</span>
-			<span class="message-to">${messages[i].to}:</span>
-			<span class="message-text">${messages[i].text}</span>				
+				<span class="message-time">(${messages[i].time})</span>
+				<span class="message-from">${messages[i].from}</span>
+				<span>para</span>
+				<span class="message-to">${messages[i].to}:</span>
+				<span class="message-text">${messages[i].text}</span>				
 			</li>
 			`;
 		} else if (messages[i].type === "status") {
 			message.innerHTML += `
 			<li class="message status">
-			<span class="message-time">(${messages[i].time})</span>
-			<span class="message-from">${messages[i].from}</span>
-			<span class="message-text">${messages[i].text}</span>	
+				<span class="message-time">(${messages[i].time})</span>
+				<span class="message-from">${messages[i].from}</span>
+				<span class="message-text">${messages[i].text}</span>	
 			</li>
 			`;
-		} else if (messages[i].type === "private_message" /*&& messages[i].to === myname*/) {
+		} else if (messages[i].type === "private_message" && (messages[i].to === myname || messages[i].from === myname)) {
 			message.innerHTML += `
 			<li class="message private">
-			<span class="message-time">(${messages[i].time})</span>
-			<span class="message-from">${messages[i].from}</span>
-			<span>reservadamente para</span>
-			<span class="message-to">${messages[i].to}:</span>
-			<span class="message-text">${messages[i].text}</span>
+				<span class="message-time">(${messages[i].time})</span>
+				<span class="message-from">${messages[i].from}</span>
+				<span>reservadamente para</span>
+				<span class="message-to">${messages[i].to}:</span>
+				<span class="message-text">${messages[i].text}</span>
 			</li>
 			`;
 		}
@@ -147,6 +148,51 @@ function statusErrorLogin(erro) {
 	document.querySelector(".name-enter-loading").classList.toggle("hidden");
 }
 
+function changeRecipient(user) {
+	let recipientType = user.querySelector(".recipient-type");
+	let recipient = document.querySelector(".private-text");
+	
+	if (recipientType.innerHTML === "Todos") {
+		recipient.classList.add("hidden");
+		recipient.querySelector(".name-to").innerHTML = "Todos";
+		messageRecipient = "Todos";
+	} else {
+		recipient.classList.remove("hidden");
+		recipient.querySelector(".name-to").innerHTML = recipientType.innerHTML;
+		messageRecipient = recipientType.innerHTML;
+	}
+	
+	changeCheck(user);
+}
+
+function changeVisibility(message, type) {
+	
+	let recipient = document.querySelector(".private-text");
+	
+	if (type === "private") {
+		messageVisibility = "private_message";
+		recipient.querySelector(".reserved-type-message").innerHTML = "(reservadamente)";
+	} else {
+		messageVisibility = "message";
+		recipient.querySelector(".reserved-type-message").innerHTML = "";
+	}
+	
+	changeCheck(message);
+}
+
+function changeCheck(element) {
+	let check = element.parentElement.querySelector(".check");
+	
+	if (check !== null) {
+		check.classList.add("hidden");
+		check.classList.remove("check");
+	}
+	
+	let uncheck = element.querySelector(".hidden");
+	uncheck.classList.remove("hidden");
+	uncheck.classList.add("check");
+}
+
 function activateMenu() {
 	document.querySelector(".menu").classList.toggle("hidden");
 }
@@ -158,4 +204,3 @@ function loginScreen() {
 function reloadPage() {
 	window.location.reload();
 }
-
